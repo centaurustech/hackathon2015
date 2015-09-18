30e6b4a1d39f75485a6510c6035a1543b685f443
JC Jollant <jean-cedric.jollant@misys.com>
2015-09-18 16:20:03 +0200

    Clean up

--- rest/src/main/java/com/misys/crowdfunding/rest/Ping.java
+++ rest/src/main/java/com/misys/crowdfunding/rest/Ping.java
@@ -1,11 +1,11 @@
-package com.trmsys.boat.rest;
+package com.trmsys.crowdfunding.rest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/ping")
 public class Ping {
     @GET
-    public String ping() {
+    public String pong() {
         return "pong";
     }
 }
