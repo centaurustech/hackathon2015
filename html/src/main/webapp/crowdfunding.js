19924db72a16ec57a8fb17317897a447a402989f
JC Jollant <jean-cedric.jollant@misys.com>
2015-09-22 19:00:12 +0200

    Basic project view header and transition

--- html/src/main/webapp/crowdfunding.js
+++ html/src/main/webapp/crowdfunding.js
@@ -14,25 +14,33 @@ $("#ping-button").click( function() {
 });
 
 function loadProjects() {
-    console.log("projects sent");
     $.getJSON("/api/project", function (data) {
-        console.log("projects received " + data);
         $.each(data.array, function(i,project) {
 
-            innerHTML = '<div class="grid grid_6"><div class="contentItem"><h2><a href="">' + project.name + '</a></h2>';
-            innerHTML += '<a href=""><img src="' + project.pictureURL + '" width="220" height="160"></a>';
+            // Force it for now, it should be populated soon
+            project.id = 12;
+            var refHTML = 'href="#" onclick="showProject(' + project.id + ')"';
+            var innerHTML = '<div class="grid grid_6"><div class="contentItem"><h2><a ' + refHTML + '>' + project.name + '</a></h2>';
+            innerHTML += '<a ' + refHTML + '><img src="' + project.pictureURL + '" width="220" height="160"></a>';
             innerHTML += '<p style="padding-right: 0">' + project.description + '</p>';
-            innerHTML += '<p><a class="redLink" href="" title="Find out more">Find out more</a></p>';
+            innerHTML += '<p><a class="redLink" ' + refHTML + ' title="Find out more">Find out more</a></p>';
             innerHTML += '</div>';
 
             $('#projects-list').append( innerHTML);
         });
     })
         .done(function () {
-            console.log("projects suceeded");
+//            console.log("projects suceeded");
         })
         .fail(function () {
             console.log("projects failed");
         })
     ;
+}
+
+function showProject( id) {
+//    console.log( "I should be showing project " + id);
+    $('#projects-list').hide(500);
+    // TODO load data from REST
+    $('#project-view').show(250);
 }
