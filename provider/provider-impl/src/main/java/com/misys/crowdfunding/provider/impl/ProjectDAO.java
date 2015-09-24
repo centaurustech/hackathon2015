5a67b02e4aa9ae977e7aa05e0e1c079791a1f300
Frederic Andre <frederic.andre@misys.com>
2015-09-24 18:06:48 +0200

    Implem full projects search

--- provider/provider-impl/src/main/java/com/misys/crowdfunding/provider/impl/ProjectDAO.java
+++ provider/provider-impl/src/main/java/com/misys/crowdfunding/provider/impl/ProjectDAO.java
@@ -1,11 +1,13 @@
 package com.misys.crowdfunding.provider.impl;
 
 import com.misys.crowdfunding.provider.api.IProjectDAO;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentPool;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
 
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -16,11 +18,33 @@ import java.util.Map;
 public class ProjectDAO implements IProjectDAO {
 
     @Override
-    public Map<String, Object> getProject(String id) {
-        //ODatabaseDocumentTx db = new ODatabaseDocumentTx("remote:localhost/Crowdfunding").open("admin", "admin");
-        ODatabaseDocumentTx db = new ODatabaseDocumentTx("remote:192.168.100.11/Crowdfunding").open("admin", "admin");
-        List<ODocument> result = db.query(new OSQLSynchQuery<ODocument>("select @rid.asString() as id, name, description, imgSrc, type, currency, targetAmount, currentAmount, targetDate, creationDate from projects where @rid="+id));
+    public List<Map<String, Object>> getProjects() {
+        //ODatabaseDocumentTx db = ODatabaseDocumentPool.global().acquire("remote:localhost/Crowdfunding", "admin", "admin");
+        ODatabaseDocumentTx db = ODatabaseDocumentPool.global().acquire("remote:192.168.100.11/Crowdfunding", "admin", "admin");
+        try {
+            List<ODocument> result = db.query(new OSQLSynchQuery<ODocument>("select @rid.asString() as id, name, description, imgSrc from projects"));
+            List<Map<String, Object>> ret = new ArrayList<>();
 
-        return result.get(0).toMap();
+            for(ODocument od : result) {
+                ret.add(od.toMap());
+            }
+
+            return ret;
+        } finally {
+            db.close();
+        }
+    }
+
+    @Override
+    public Map<String, Object> getProject(String id) {
+        //ODatabaseDocumentTx db = ODatabaseDocumentPool.global().acquire("remote:localhost/Crowdfunding", "admin", "admin");
+        ODatabaseDocumentTx db = ODatabaseDocumentPool.global().acquire("remote:192.168.100.11/Crowdfunding", "admin", "admin");
+        try {
+
+            List<ODocument> result = db.command(new OSQLSynchQuery<ODocument>("select @rid.asString() as id, name, description, imgSrc, type, currency, targetAmount, currentAmount, targetDate, creationDate from projects where @rid=?")).execute(id);
+            return result.get(0).toMap();
+        } finally {
+            db.close();
+        }
     }
 }
--- service/service-impl/src/main/java/com/misys/crowdfunding/service/impl/ProjectService.java
+++ service/service-impl/src/main/java/com/misys/crowdfunding/service/impl/ProjectService.java
@@ -9,6 +9,7 @@ import javax.inject.Inject;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.List;
 import java.util.Map;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -28,52 +29,21 @@ public class ProjectService implements IProjectService {
 
     @Override
     public String getProjects() {
-        // TODO call DAO and process output to json
+        List<Map<String, Object>> data = projectDAO.getProjects();
 
-        return "[\n" +
-                "  {\n" +
-                "    \"id\": \"#12:0\",\n" +
-                "    \"name\": \"CrowdFunding\",\n" +
-                "    \"description\": \"Our fantastic project that will revolutionize the world\",\n" +
-                "    \"imgSrc\": \"https://resistanceinventerre.files.wordpress.com/2013/10/asumo2.jpg?w=300&h=277\"\n" +
-                "  },\n" +
-                "  {\n" +
-                "    \"id\": \"2\",\n" +
-                "    \"name\": \"Save the rock\",\n" +
-                "    \"description\": \"Some concrete funding !\",\n" +
-                "    \"imgSrc\": \"http://www.megaconnard.com/wp-content/uploads/2011/02/Darwin.jpg\"\n" +
-                "  },\n" +
-                "  {\n" +
-                "    \"id\": \"3\",\n" +
-                "    \"name\": \"New coffee brand\",\n" +
-                "    \"description\": \"We need more cafein in our sweet beverage\",\n" +
-                "    \"imgSrc\": \"http://rlv.zcache.fr/cafe_de_boissons_faites_les_choses_stupides_carte_postale-r049dd921942847f684201d09f0081ad8_vgbaq_8byvr_324.jpg\"\n" +
-                "  },\n" +
-                "  {\n" +
-                "    \"id\": \"4\",\n" +
-                "    \"name\": \"Waterproof ipad\",\n" +
-                "    \"description\": \"An underwater version of the well known device\",\n" +
-                "    \"imgSrc\": \"http://www.lebonbuzz.net/wp-content/uploads/2015/09/354.jpg\"\n" +
-                "  },\n" +
-                "  {\n" +
-                "    \"id\": \"5\",\n" +
-                "    \"name\": \"Shoes umbrella\",\n" +
-                "    \"description\": \"Never wet again... ever\",\n" +
-                "    \"imgSrc\": \"http://tooply.com/photo/item/570/5001581126219.jpg\"\n" +
-                "  },\n" +
-                "  {\n" +
-                "    \"id\": \"6\",\n" +
-                "    \"name\": \"Ecolosmoke\",\n" +
-                "    \"description\": \"Smoking CAN be ecological !\",\n" +
-                "    \"imgSrc\": \"http://tooply.com/photo/item/570/4375366211217.jpg\"\n" +
-                "  },\n" +
-                "  {\n" +
-                "    \"id\": \"7\",\n" +
-                "    \"name\": \"Pastafarianism\",\n" +
-                "    \"description\": \"In pasta we trust\",\n" +
-                "    \"imgSrc\": \"http://kevenroy.ca/wp-content/uploads/2015/02/une-pastafarianisme-660x330.jpg\"\n" +
-                "  }\n" +
-                "]";
+        for (Map<String, Object> m : data) {
+            m.remove("@rid");
+        }
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'"));
+
+            return objectMapper.writeValueAsString(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 
     @Override
